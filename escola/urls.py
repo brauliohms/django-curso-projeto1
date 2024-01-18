0af1fd473e10b2038e6b26e1880ce8b518eae212
Braulio Henrique Marques Souto <braulio@disroot.org>
2024-01-18 18:45:52 -0300

    Criado novos paths para realizar o CRUD em alunos

--- escola/urls.py
+++ escola/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from escola.views import alunosList, home
+from escola.views import alunosDelete, alunosList, alunosNovo, alunosUpdate, home
 
 urlpatterns = [
-    path("", home),
-    path("alunos/", alunosList),
+    path("", home, name="escola"),
+    path("alunos/", alunosList, name="alunosList"),
+    path("alunos/novo", alunosNovo, name="alunosNovo"),
+    path("alunos/<int:id>", alunosUpdate, name="alunosUpdate"),
+    path("alunos/delete/<int:pk>", alunosDelete, name="alunosDelete"),
 ]
