0c386144de9ec0bfd0cf3a9c61f4fb3881968ced
Braulio Henrique Marques Souto <braulio@disroot.org>
2024-01-18 18:48:17 -0300

    Criado o CRUD em Aluno

--- escola/views.py
+++ escola/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import AlunoForm
+from .models import Aluno
 
 
 # Create your views here.
@@ -9,4 +12,36 @@ def home(request):
 
 
 def alunosList(request):
-    return render(request, "escola/lista.html")
+    alunosListaRecebida = Aluno.objects.all()
+    return render(request, "escola/lista.html", {"alunos": alunosListaRecebida})
+
+
+def alunosNovo(request):
+    form = AlunoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect("alunosList")
+
+    return render(request, "escola/aluno_novo.html", {"formAlunoNovo": form})
+
+
+def alunosUpdate(request, id):
+    aluno = Aluno.objects.get(pk=id)
+
+    form = AlunoForm(request.POST or None, instance=aluno)
+
+    if form.is_valid():
+        form.save()
+        return redirect("alunosList")
+
+    return render(
+        request, "escola/aluno_novo.html", {
+            "formAlunoNovo": form, "aluno": aluno}
+    )
+
+
+def alunosDelete(request, pk):
+    aluno = Aluno.objects.get(pk=pk)
+    aluno.delete()
+    return redirect("alunosList")
